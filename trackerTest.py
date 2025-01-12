1ba94f6bc04b7795b21d1fb6edb33a1889cdf6fc
PrashantNeupane33 <www.panbr123@gmail.com>
2025-01-12 14:58:46 +0545

    Testing detector and tracker module

--- trackerTest.py
+++ trackerTest.py
@@ -1,5 +1,4 @@
-import detector
-import tracker
+import Detector
 import cv2 as cv
 import sys
 sys.path.append('../depthPY')
@@ -15,6 +14,28 @@ while True:
     if not isTrue:
         break
     if detect:
-        detector = detector(model_path)
-        detections = detector.preditct(frame)
+        detect = Detector.detector(model_path)
+        detections = detect.predict(frame)
         print(detections)
+        if detections is not None:
+            cx, cy, w, h = detections
+            cv.rectangle(frame, (int(cx-w/2), int(cy-h/2)),
+                         (int(cx+w/2), int(cy+h/2)), (0, 255, 0), 2)
+            detect = False
+            tracker = cv.TrackerCSRT_create()
+            bbox_tracker = (
+                int(cx - w / 2), int(cy - h / 2), int(w), int(h))
+            tracker.init(frame, bbox_tracker)
+            break
+
+    else:
+        cv.imshow("Balls", frame)
+        success, bbox = tracker.update(frame)
+        if success:
+            x, y, w, h = [int(v) for v in bbox]
+            cv.rectangle(frame, (x, y),
+                         (x + w, y + h), (0, 255, 0), 2)
+            count += 1
+            if count > 100:
+                detect = True
+                count = 0
